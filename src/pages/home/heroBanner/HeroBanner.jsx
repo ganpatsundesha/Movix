88424fbc9e6375bb121ed6c486637a4323a81289
ganpatsundesha <ganpatsundesha47@gmail.com>
2024-01-30 13:35:44 +0530

    path change

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import "./style.scss"
 import useFetch from '../../../hooks/useFetch'
-import Img from '../../../components/lazyLoadImage/Img'
+import Img from '../../../components/lazyLoadImage/Img.jsx'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import { useLogInData } from '../../../context/LoginData'
 

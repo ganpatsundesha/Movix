9f7149ed5628ede1a03b4e2553cf7a4d5edee8a2
ganpatsundesha <ganpatsundesha47@gmail.com>
2024-01-30 13:14:28 +0530

    path changes of Tranding comp in home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./style.scss"
 import HeroBanner from './heroBanner/HeroBanner'
-import Tranding from './tranding/tranding'
+import Tranding from './tranding/Tranding'
 import Popular from "./popular/Popular"
 import TopRated from './topRated/TopRated'
 
